45d1a672b390dc0902da809634a76da464ca65c9
Alexey Golota <agolota@corp.web4pro.com.ua>
2020-05-15 15:24:52 +0300

    Init extension

---
+++ readme.md
@@ -0,0 +1,6 @@
+W4PLEGO BaseIntegration
+=====================
+
+[wiki link](https://pm.web4pro.com.ua/projects/lego-web4pro-baseintegration/wiki)
+
+### VERSION 1.0.0
---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'W4PLEGO_Base',
+    __DIR__
+);
